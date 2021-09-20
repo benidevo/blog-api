5aa44bf8c06bb1cbfc8ee6d43efd3ed284d60bbc
Benjamin Idewor <benjaminidewor@gmail.com>
2021-09-20 09:04:11 +0100

    fix: handle unhandled routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
-const { success } = require('./app/utils/apiResponse');
+const { success, error } = require('./app/utils/apiResponse');
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.json({ extended: false }));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, PUT, DELETE');
     next();
 });
 
@@ -25,6 +25,11 @@ app.get('/', (req, res) => {
 // routes
 app.use('/api/auth', require('./routes/users'));
 app.use('/api/blog-posts', require('./routes/posts'));
+
+// handle unhandled routes
+app.all('*', (req, res) => {
+    res.status(404).json(error(`${req.originalUrl} route does not exist on this server`));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
